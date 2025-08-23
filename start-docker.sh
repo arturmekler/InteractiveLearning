7cf53840f9d3d2c3ba246152cada593d25fe2c3d
arturmekler <artur.mekler@gmail.com>
2025-08-23 12:00:47 +0200

    docker files

---
+++ start-docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Budowanie i uruchamianie aplikacji w Docker..."
+
+# Utwórz katalog dla certyfikatów HTTPS jeśli nie istnieje
+mkdir -p ~/.aspnet/https
+
+# Sprawdź czy certyfikat istnieje
+if [ ! -f ~/.aspnet/https/aspnetapp.pfx ]; then
+    echo "Tworzenie certyfikatu HTTPS..."
+    
+    # Najpierw wyczyść stare certyfikaty
+    dotnet dev-certs https --clean
+    
+    # Wygeneruj i eksportuj nowy certyfikat
+    dotnet dev-certs https -ep ~/.aspnet/https/aspnetapp.pfx -p YourPassword123
+    
+    # Ufaj certyfikatowi
+    dotnet dev-certs https --trust
+fi
+
+# Uruchom z HTTPS
+docker-compose -f docker-compose.https.yml up --build
